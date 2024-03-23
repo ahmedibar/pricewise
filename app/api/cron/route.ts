b57ee4e88f79fb1ac887f84ee9d2a9459d1887b2
Ahmed Nur Ali <88971150+ahmedibar@users.noreply.github.com>
2024-03-24 01:58:34 +0530

    Fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,10 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export const GET = async () => {
   try {
     connectToDB();
@@ -40,7 +44,7 @@ export const GET = async () => {
 
         const updatedProduct = await Product.findOneAndUpdate(
           {
-            url: scrapedProduct.url,
+            url: product.url,
           },
           product
         );
